a3b9e7f87d0971897226f4dfbf0b47c7b64b658b
Max Parry <66670928+Buggem@users.noreply.github.com>
2023-04-14 16:52:31 +1000

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,5 @@
+document.getElementById("bigCookie").addEventListener("click", function() {
+setInterval(function(){for(let i = 0; i < 19; i++){Game.Earn(Infinity); document.getElementById("product" + i).click();} Game.Earn(Infinity); document.getElementById("upgrade0").click();}, 100)
+});document.getElementById("bigCookie").addEventListener("click", function() {
+setInterval(function(){for(let i = 0; i < 19; i++){Game.Earn(Infinity); document.getElementById("product" + i).click();} Game.Earn(Infinity); document.getElementById("upgrade0").click();}, 100)
+});
