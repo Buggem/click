9dbc0faf078ebde124dbb9b6712060ea5d5431c9
Max Parry <66670928+Buggem@users.noreply.github.com>
2023-04-19 17:25:01 +1000

    fix copy paste 2 times L bozo

--- gg.js
+++ gg.js
@@ -1,5 +1,3 @@
 document.getElementById("bigCookie").addEventListener("click", function() {
 setInterval(function(){for(let i = 0; i < 19; i++){Game.Earn(Infinity); document.getElementById("product" + i).click();} Game.Earn(Infinity); document.getElementById("upgrade0").click();}, 100)
-});document.getElementById("bigCookie").addEventListener("click", function() {
-setInterval(function(){for(let i = 0; i < 19; i++){Game.Earn(Infinity); document.getElementById("product" + i).click();} Game.Earn(Infinity); document.getElementById("upgrade0").click();}, 100)
 });
