75add76875a66ce3cd8df0393c8fa0783cf0ed02
Max Parry <66670928+Buggem@users.noreply.github.com>
2025-02-08 15:56:02 +1100

    toggling

--- universal-hax.js
+++ universal-hax.js
@@ -1,20 +1,28 @@
 // Site-specific Configuration settings
+window.clickerHaxCPS = 50;
 window.bigcookie = null;
 window.upgrades = null;
+window.haxInterval = -1;
+
 if(window.location.href.includes("stimulation")) {
-  bigcookie = document.getElementsByClassName("main-btn")[0];
-  upgrades = document.getElementsByClassName("main-upgrades")[0];
+  window.bigcookie = document.getElementsByClassName("main-btn")[0];
+  window.upgrades = document.getElementsByClassName("main-upgrades")[0];
 }
 
-function hacks() {
-  setInterval(
-    function() {
-      for(let i = 0; i < 50; i++) {
-        bigcookie.click();
-      } 
-      for(let upgrade of upgrades.children) {
-        upgrade.click();
-      }
-    },
-  100);
+function toggleClickerHax() {
+  if(window.haxInterval == -1) {
+    window.haxInterval = setInterval(
+      function() {
+        for(let i = 0; i < window.clickerHaxCPS; i++) {
+          window.bigcookie.click();
+        } 
+        for(let upgrade of window.upgrades.children) {
+          upgrade.click();
+        }
+      },
+    100);
+  } else {
+    clearInterval(window.haxInterval);
+    haxInterval = -1;
+  }
 } 
